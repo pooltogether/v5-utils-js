7f0484918b204c8d3b135c73e95fed3313abd4f9
Charles Bergeron <chuck.bergeron@gmail.com>
2023-05-09 14:48:48 -0700

    update to add mumbai support

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,9 @@
 const CHAIN_ID = {
   goerli: 5,
+  mumbai: 80001
 };
 
 export const TWAB_CONTROLLER_SUBGRAPH_URIS = {
   [CHAIN_ID.goerli]: `https://api.thegraph.com/subgraphs/name/pooltogether/v5-eth-goerli-twab-controller`,
+  [CHAIN_ID.mumbai]: `https://api.thegraph.com/subgraphs/name/pooltogether/v5-polygon-mumbai-twab-controller`
 };
