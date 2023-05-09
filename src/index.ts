eb8dee4fd1d6ab3855617c5c15907ad0de534a8b
Charles Bergeron <chuck.bergeron@gmail.com>
2023-05-09 15:12:18 -0700

    more necessary config changes to build properly with tsc as esm module instead of commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import * as compute from "./compute";
-import * as utils from "./utils";
 export * from "./compute";
 export * from "./utils";
 export * from "./types";
-export { compute, utils };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,5 +5,5 @@ const CHAIN_ID = {
 
 export const TWAB_CONTROLLER_SUBGRAPH_URIS = {
   [CHAIN_ID.goerli]: `https://api.thegraph.com/subgraphs/name/pooltogether/v5-eth-goerli-twab-controller`,
-  [CHAIN_ID.mumbai]: `https://api.thegraph.com/subgraphs/name/pooltogether/v5-polygon-mumbai-twab-controller`
+  [CHAIN_ID.mumbai]: `https://api.thegraph.com/subgraphs/name/pooltogether/v5-polygon-mumbai-twab-control`
 };
