68c91358eb9bd2a308685a4dab17c00cbf40f2d3
Charles Bergeron <chuck.bergeron@gmail.com>
2023-05-10 12:51:54 -0700

    move getContract(s) utility functions and the testnet contracts blob json to here

---
+++ src/utils/getContract.ts
@@ -0,0 +1,19 @@
+import { Contract } from "ethers";
+
+import { getContracts } from "./getContracts";
+import { ContractsBlob } from "../types";
+
+// Returns the first contract that matches the params by name, chain, and contract version
+export function getContract(
+  name: string,
+  chainId: number,
+  providerOrSigner: any,
+  contractsBlob: ContractsBlob,
+  version = {
+    major: 1,
+    minor: 0,
+    patch: 0,
+  }
+): Contract | undefined {
+  return getContracts(name, chainId, providerOrSigner, contractsBlob, version)[0];
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+export * from "./getContract";
+export * from "./getContracts";
 export { getWinnersClaims } from "./getWinnersClaims";
 export { getSubgraphVaults } from "./getSubgraphVaults";
 export * from "./constants";
